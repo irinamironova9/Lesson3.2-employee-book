336fdebea1721081d8dd327c95e419d30183c9e2
Ирина <mi.irene97@gmail.com>
2022-11-18 15:02:25 +0300

    Initial commit

---
+++ src/main/java/skypro/course3/lesson32employeebook/Lesson32EmployeeBookApplication.java
@@ -0,0 +1,13 @@
+package skypro.course3.lesson32employeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson32EmployeeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson32EmployeeBookApplication.class, args);
+    }
+
+}
